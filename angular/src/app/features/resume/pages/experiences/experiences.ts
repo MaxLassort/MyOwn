3ec31d2c88b8709f3048a5fedf9f12931ae8b630
Maxime <maximelassort@gmail.com>
2026-01-19 13:35:07 +0100

    feat: education page

--- angular/src/app/features/resume/pages/experiences/experiences.ts
+++ angular/src/app/features/resume/pages/experiences/experiences.ts
@@ -2,13 +2,15 @@ import {Component} from '@angular/core';
 import {Experience, EXPERIENCES, TECHNOLOGIES, TechnologyCard,} from '../../../../core/models/experience.model';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {ExperienceItem} from './components/experience-item/experience-item.component';
+import {NgClass} from '@angular/common';
 
 
 @Component({
   selector: 'app-experiences',
   imports: [
     ExperienceItem,
-    FaIconComponent
+    FaIconComponent,
+    NgClass
   ],
   templateUrl: './experiences.html'
 })
